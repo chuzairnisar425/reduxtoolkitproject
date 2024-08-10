596e0556fbe462378a76eb82ac6160a1ba3a3fa3
Muhammad Uzair Nisar <uzair.nisar425@gmail.comm>
2024-08-10 11:07:48 +0500

    modified

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+
+    <h2 className='heading'>Welcome to tthe redux toolkit store</h2>
+    <section>
+        <h3>PRODUCTS</h3>
+      
+    </section>
+    </div>
+  );
+}
+
+export default Home;
