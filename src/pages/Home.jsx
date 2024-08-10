bd0fb97512382b88007ffd3aba0c073b30ea2efd
Muhammad Uzair Nisar <uzair.nisar425@gmail.comm>
2024-08-10 11:27:24 +0500

    modified

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Products from '../components/Products';
 
 const Home = () => {
   return (
@@ -7,7 +8,7 @@ const Home = () => {
     <h2 className='heading'>Welcome to tthe redux toolkit store</h2>
     <section>
         <h3>PRODUCTS</h3>
-      
+      <Products/>
     </section>
     </div>
   );
